3b274d5969910f994cf9ee76f961328df533d072
Aryanftw <ap890383@gmail.com>
2024-09-16 12:46:07 +0530

    styles

--- src/Square.jsx
+++ src/Square.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Square({value,onClick}) {
   return (
     <>
-    <div className = 'square-container' style = {{border : "1px solid black", height: "200px", width: '100%', display:'flex',justifyContent:'center',alignItems:'center'}}
+    <div className = 'square-container' style = {{border : "1px solid white", height: "200px", width: '100%', display:'flex',justifyContent:'center',alignItems:'center'}}
     onClick={onClick}
     >{value}</div>
     </>
